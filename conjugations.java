63a403cc0fe48ba6bcc076594df08b4273a7505c
Bluskyfishing <blusky445@gmail.com>
2024-09-06 20:37:27 +0200

    n5 verb list finalized, sstarting on present conjugation.

---
+++ conjugations.java
@@ -0,0 +1,20 @@
+public class conjugations {
+    public static String[] present(String kanji, String pos){
+        String informalPositive = kanji;
+
+        if (pos == "Ichidan verb"){
+            char kanjiStem = kanji.charAt(0); //naive way, maybe check all letters if kanji? (unicode)
+
+            
+            
+            //String informalNegative;
+            //String formalPositive;
+            //String formalPositive;
+
+        }
+
+
+        return null;
+    }
+    
+}
